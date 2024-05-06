0c4e8280774c4875ab237968c14df04b0904cc02
luckylinux <luckylinux>
2024-05-06 13:01:03 +0200

    Switching to using Library of Container Builds using https://github.com/luckylinux/container-build-tools.

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing ${scriptpath}/${relativepath}); fi
+
+# Load Configuration
+libpath=$(readlink --canonicalize-missing "${toolpath}/includes")
+source ${libpath}/functions.sh
+
+
+# Optional argument
+engine=${1-"podman"}
+
 # Container Name
-name="docker-registry-tools"
+containername="container-registry-tools"
+
+# Container Image
+containerimage="container-registry-tools:debian-latest"
 
 # Containers Configuration Folder to Map
 containersconfigfolder="./containers"
@@ -10,12 +25,15 @@ containersconfigfolder="./containers"
 eval "$(shdotenv --env .env || echo \"exit $?\")"
 
 # Terminate and Remove Existing Containers if Any
-podman stop --ignore ${name}
-podman rm --ignore ${name}
+${engine} stop --ignore ${containername}
+${engine} rm --ignore ${containername}
 
 # Run Image with Infinite Loop to prevent it from automatically terminating
-podman run -d --name=${name} --env-file "./.env" -v "${containersconfigfolder}:/etc/containers" "localhost/docker-registry-tools:latest"
+${engine} run -d --name=${containername} --env-file "./.env" -v "${containersconfigfolder}:/etc/containers" localhost:5000/local/"${containerimage}"
+
+# Open Interactive Shell with Container
+${engine} exec -it ${containername} /bin/bash
 
 # Sync One Image
-#podman exec "${name}" skopeo sync --scoped --src docker --dest docker --all ghcr.io/home-assistant/home-assistant:stable "${LOCAL_MIRROR}" # Double Quotes means that the value from the HOST Shell will be used
-#podman exec "${name}" skopeo sync --scoped --src "docker" --dest "docker" --all "ghcr.io/home-assistant/home-assistant:stable" '${LOCAL_MIRROR}'    # Single Quotes means that the value from the CONTAINER Shell will be used
+#${engine} exec "${containername}" skopeo sync --scoped --src docker --dest docker --all ghcr.io/home-assistant/home-assistant:stable "${LOCAL_MIRROR}" # Double Quotes means that the value from the HOST Shell will be used
+#${engine} exec "${containername}" skopeo sync --scoped --src "docker" --dest "docker" --all "ghcr.io/home-assistant/home-assistant:stable" '${LOCAL_MIRROR}'    # Single Quotes means that the value from the CONTAINER Shell will be used
