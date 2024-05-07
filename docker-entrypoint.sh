42ada24ab1f41179778bf601fdf58ef6d68b7f4c
luckylinux <luckylinux>
2024-05-07 20:47:46 +0200

    Fix typo for pid in docker-entrypoint.sh.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,10 @@ cd /opt/app
 # Start the main process and save its PID
 # Use exec to replace the shell script process with the main process
 exec /opt/app/app.sh &
-pid=$!
+pidh=$!
 
 # Trap the SIGTERM signal and forward it to the main process
-trap 'kill -SIGTERM $pid; wait $pid' SIGTERM
+trap 'kill -SIGTERM $pidh; wait $pidh' SIGTERM
 
 # Wait for the main process to complete
 wait $pidh
